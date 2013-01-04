e66949f32705b8a0268b60756c56a342c1e47cb4
sebvandijk <sebvandijk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-04 10:54:42 +0000

    extra update, forgot version number
    git-svn-id: https://plugins.svn.wordpress.org/top-authors/trunk@647819 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- top_authors.php
+++ top_authors.php
@@ -3,7 +3,7 @@
  * Plugin Name: Top Authors
  * Plugin URI: http://developr.nl/work/top-authors
  * Description: A highly customizable widget that sums the top authors(most contributing) on your blog
- * Version: 0.5.5
+ * Version: 0.5.6
  * Author: developR | Seb van Dijk
  * Author URI: http://www.developr.nl
  *
